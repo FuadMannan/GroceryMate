8542930363d65fced1dbea26ecf016b171cb0d44
Sanket Patel <sanket@Sankets-MacBook-Pro.local>
2024-02-03 14:01:10 -0500

    Add Signup Page

--- GroceryMate/myapp/urls.py
+++ GroceryMate/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path(r'signup/', views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
