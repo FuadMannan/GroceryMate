69dd9eefc0fbcb1c8a7b7703c16efe3d232c24b9
Sanket Patel <sanket@Sankets-MacBook-Pro.local>
2024-02-14 17:01:33 -0500

    Make signup page as default page

--- GroceryMate/myapp/urls.py
+++ GroceryMate/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.signup, name='signup'),
     path(r'signup/', views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- GroceryMate/myapp/views.py
+++ GroceryMate/myapp/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import render, redirect
 
 from myapp.forms import SignUpForm
 
-
-def home(request):
-    return render(request, "home.html")
-
-
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
