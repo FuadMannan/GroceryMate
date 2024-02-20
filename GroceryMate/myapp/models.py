5ec19426e8a4e5a072968d1fd7890650ebe701f9
Sanket Patel <sanket@Sankets-MacBook-Pro.local>
2024-02-20 18:15:05 -0500

    Fix formatting

--- GroceryMate/myapp/models.py
+++ GroceryMate/myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Products(models.Model):
@@ -8,17 +9,20 @@ class Products(models.Model):
     ProductName = models.CharField(max_length=255)
     Category = models.CharField(max_length=255, null=True)
 
+
 class Stores(models.Model):
     StoreID = models.BigAutoField(primary_key=True, verbose_name='Store ID')
     ChainName = models.CharField(max_length=255)
     StoreName = models.CharField(max_length=255)
     Location = models.CharField(max_length=255, unique=True)
 
+
 class Prices(models.Model):
     PriceID = models.BigAutoField(primary_key=True, verbose_name='Price ID')
     ProductID = models.ForeignKey(Products, on_delete=models.CASCADE)
     StoreID = models.ForeignKey(Stores, on_delete=models.CASCADE)
     Price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+
 
 class NutritionalInfo(models.Model):
     NutritionID = models.BigAutoField(primary_key=True, verbose_name='Nutrition ID')
@@ -35,11 +39,13 @@ class NutritionalInfo(models.Model):
     Fiber = models.DecimalField(max_digits=7, decimal_places=2)
     Sugar = models.DecimalField(max_digits=7, decimal_places=2)
 
+
 class GroceryLists(models.Model):
     ListID = models.BigAutoField(primary_key=True, verbose_name='List ID')
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
     ListName = models.CharField(max_length=255)
     DateCreated = models.DateField(auto_now_add=True)
+
 
 class ListItems(models.Model):
     ItemID = models.BigAutoField(primary_key=True, verbose_name='List Item ID')
