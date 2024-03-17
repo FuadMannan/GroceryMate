646103d845cc9b29764c1990eaaf6d3c47968bda
FuadMannan <fuad.mannan@gmail.com>
2024-03-17 17:00:48 -0400

    Update scheduled scraping with refactored scrapers

--- GroceryMate/myapp/tasks.py
+++ GroceryMate/myapp/tasks.py
@@ -4,9 +4,10 @@ from myapp.backend.scrape_api import scrape_api as scrape
 @shared_task(bind=True)
 def scrape_task(self):
     print('in celery')
-    scrape.Locations.get_Walmart()
-    scrape.Locations.get_Loblaws_brands('Loblaws')
-    scrape.Locations.get_Loblaws_brands('No Frills')
-    scrape.ProductPrices.get_Walmart()
-    scrape.ProductPrices.get_Loblaws_brands('Loblaws')
-    scrape.ProductPrices.get_Loblaws_brands('No Frills')
+    for i, chain in enumerate(scrape.CHAIN_NAMES[1:]):
+        if i <= 5:
+            scraper = scrape.LoblawsBrands(chain)
+        else:
+            scraper = scrape.Metro()
+        scraper.get_locations()
+        scraper.get_products_prices()
