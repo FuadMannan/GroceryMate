ff5c624c6c9e46ff36d8ef918f558e8cd8198d65
FuadMannan <fuad.mannan@gmail.com>
2024-03-16 20:36:12 -0400

    Add scraping for some Loblaws brands and Metro

--- GroceryMate/myapp/backend/scrape_api/scrape_api.py
+++ GroceryMate/myapp/backend/scrape_api/scrape_api.py
@@ -1,3 +1,4 @@
+import requests
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import StaleElementReferenceException
 from seleniumbase import Driver
@@ -5,6 +6,7 @@ from abc import ABC, abstractmethod
 import decimal
 import time
 import re
+import json
 
 from ...models import Chains, Stores, Products, Prices
 from django.db.utils import IntegrityError
@@ -14,6 +16,11 @@ CHAIN_NAMES = [
     'Walmart',
     'Loblaws',
     'No Frills',
+    'Real Canadian Superstore',
+    'Wholesale Club',
+    'Valu-mart',
+    'Fortinos',
+    'Metro'
 ]
 
 
@@ -121,8 +128,10 @@ class Scraper(ABC):
     def get_locations(self):
         self.attempt(self.location_actions)
 
+
     def get_products_prices(self):
         self.attempt(self.grocery_actions)
+
 
     def attempt(self, actions):
         try:
@@ -222,3 +231,70 @@ class LoblawsBrands(Scraper):
                         product_price = decimal.Decimal(product_price)
                         self.save_price(product, product_price)
                     more_pages = self.navigate_next('button[aria-label="Next Page"]')
+
+
+class Metro(Scraper):
+
+
+    def __init__(self, load_cities_necessary=False) -> None:
+        super().__init__(load_cities_necessary)
+        self.chain = Chains.objects.get(ChainName='Metro')
+        self.grocery_URL = 'https://www.metro.ca/en/'
+        self.locator_URL = f'{self.grocery_URL}find-a-grocery'
+        self.location_actions = (self.loop_locations,)
+
+
+    def loop_locations(self):
+        page = requests.post(self.locator_URL)
+        self.soup = BeautifulSoup(page.content, 'html.parser')
+        cities_var = self.soup.select('div#content-temp script')[2]
+        cities_str = cities_var.text
+        i1, i2 = cities_str.find('"{'), cities_str.find('}";') + 2
+        cities_str = cities_str[i1:i2].encode().decode('unicode_escape').strip('"')
+        cities_dict = json.loads(cities_str)
+        for province in cities_dict:
+            for city in cities_dict[province]:
+                for i in range(3):
+                    page = requests.post(self.locator_URL, data={'provinceCode': province, 'city': city})
+                    if page.status_code == 200:
+                        break
+                    else:
+                        time.sleep(70)
+                soup = BeautifulSoup(page.content, 'html.parser')
+                results = soup.select('div.white-wrapper')
+                for result in results:
+                    name = result.select_one('p.store-name').text
+                    location = ", ".join([i.text.strip('\n') for i in result.select('div[class^="address--line"]')])
+                    self.print_city(name, location)
+                    self.save_store(name, location)
+
+
+    def loop_grocery_items(self):
+        def get_aisle_url(aisle_name: str):
+            return f'{self.grocery_URL}online-grocery/aisles/{aisle_name.lower().replace(" & ", "-").replace(" ", "-")}'
+        self.update_page_soup()
+        if len(self.soup.select('button#onetrust-reject-all-handler')) >= 1:
+            self.driver.click('button#onetrust-reject-all-handler')
+        if self.soup.select_one('html').attrs['lang'] == 'fr-CA':
+            self.driver.click('.js-change-my-language')
+        aisle_css = '.menu-toggle-nav.left-sidebar-sub--menu.aisles--menu.inactive>.nav--container>.sub--nav--first>ul>li'
+        aisle_names = [i.text.strip() for i in self.soup.select(aisle_css)]
+        aisle_links = [get_aisle_url(aisle_name) for aisle_name in aisle_names[:16]]
+        for aisle in aisle_links:
+            self.driver.open(aisle)
+            more_pages = True
+            while more_pages:
+                self.update_page_soup()
+                items = self.soup.select('.pt__content--wrap')
+                for item in items:
+                    product_name = item.select_one('.head__title').text
+                    product = self.save_product(product_name)
+                    product_price = item.select('.price-update')
+                    if len(product_price) == 1:
+                        product_price = product_price[0].text.strip('$')
+                    else:
+                        print(f"{item.select_one('.pricing__secondary-price span').text}\n")
+                        product_price = re.findall('\d+\.?\d+', item.select_one('.pricing__secondary-price span').text)[0]
+                    product_price = decimal.Decimal(product_price)
+                    self.save_price(product, product_price)
+                more_pages = self.navigate_next('a[aria-label="Next"]')
