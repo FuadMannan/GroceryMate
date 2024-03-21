1ee852f8f89b3d0f02b1e8a83f6bd35dd3f20dac
FuadMannan <fuad.mannan@gmail.com>
2024-03-21 12:38:05 -0400

    Initialize 'None' Brand

--- GroceryMate/myapp/backend/scrape_api/__init__.py
+++ GroceryMate/myapp/backend/scrape_api/__init__.py
@@ -1,8 +1,14 @@
-from myapp.models import Chains
+from myapp.models import Chains, Brands
 from .scrape_api import CHAIN_NAMES
 
+# Initialize chains
 if Chains.objects.count() != len(CHAIN_NAMES):
     for name in CHAIN_NAMES:
         if not Chains.objects.filter(ChainName=name).exists():
             new_chain = Chains(ChainName=name)
             new_chain.save()
+
+# Initialize 'None' brand
+if Brands.objects.count() == 0:
+    brand = Brands(BrandName='None')
+    brand.save()
